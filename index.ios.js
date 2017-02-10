3cab93f0025f428c1624ca284cad1f44a097d44e
Jianyu Li <jianyu.li@feisystems.com>
2017-02-09 23:31:44 -0800

    update ios index
    update ios index

--- index.ios.js
+++ index.ios.js
@@ -4,44 +4,83 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
 import UserList from './components/UserList';
 import AddUser from './components/AddUser';
 
 export default class ReactNativeFirst extends Component {
-  
-    constructor(props) {
-        super(props);
-        this.state = {
-            openAddUser: false,
-            users:[]
-        }
-        this.openAddUser =
-            this.openAddUser.bind(this);
-    }
 
-    openAddUser(visible){
-      this.setState({openAddUser:visible});
+  constructor(props) {
+    super(props);
+    this.state = {
+      openAddUser: false,
+      users: [],
+      ip: '',
+      reloadUsers: false
     }
+    this.openAddUser = this
+      .openAddUser
+      .bind(this);
+    this.loadUsers = this
+      .loadUsers
+      .bind(this);
+  }
+
+  openAddUser(visible) {
+    this.setState({openAddUser: visible});
+  }
+
+  loadUsers() {
+    this.setState({
+      reloadUsers: !this.state.reloadUsers
+    });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <UserList users={this.state.users}/>
-        <AddUser onClose={(users)=>{this.setState({users:users||this.state.users});this.openAddUser(false);}} visible={this.state.openAddUser}/>
-        <Button
-          color="#841584"
-          onPress={()=>{this.openAddUser(true);}}
-          title="Add User"
-          accessibilityLabel="add a user to server"
-        />
+        <TextInput
+          style={{
+          fontSize: 30
+        }}
+          keyboardType="numeric"
+          placeholder='IP and port - "ip:port"'
+          onChangeText={(ip) => this.setState({ip})}
+          value={this.state.ip}/>
+
+        <UserList reloadUsersFlag={this.state.reloadUsers} ip={this.state.ip}/>
+        <AddUser
+          ip={this.state.ip}
+          onClose={(users) => {
+          this.setState({
+            users: users || this.state.users
+          });
+          this.openAddUser(false);
+        }}
+          visible={this.state.openAddUser}/>
+        <TouchableHighlight
+          style={styles.fullWidthButton}
+          onPress={() => {
+          this.loadUsers();
+        }}>
+          <Text style={styles.fullWidthButtonText}>Load Users</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={styles.fullWidthButton}
+          onPress={() => {
+          this.openAddUser(true);
+        }}>
+          <Text style={styles.fullWidthButtonText}>Add User</Text>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -50,8 +89,20 @@ export default class ReactNativeFirst extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
+  fullWidthButton: {
+    backgroundColor: 'blue',
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 2
+  },
+  fullWidthButtonText: {
+    fontSize: 24,
+    color: 'white'
+  }
 });
 
 AppRegistry.registerComponent('ReactNativeFirst', () => ReactNativeFirst);
